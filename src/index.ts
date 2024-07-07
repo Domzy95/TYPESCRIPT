91887aecc3d8dd7be2452ac14c46760dfa368a83
Domen <domen.sinkovecgmail.com>
2024-07-07 22:47:50 +0200

    todo-app

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,46 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+// import { v4 as uuidv4 } from "uuid";
+// console.log(uuidv4());
 
-import confetti from 'canvas-confetti';
+import { v4 as uuidv4 } from "uuid";
+//!ta task vnesem v spodnjo funckijo addlistitem
+type Task = {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: number;
+};
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const list = document.querySelector<HTMLUListElement>("#list");
+const from = document.getElementById("new-task-form") as HTMLFormElement | null;
+const input = document.querySelector<HTMLInputElement>("#new-task-title");
+const task: Task[] = [];
+//!če je prazen okenček brez form elemnta napiše please add task
+from?.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (input?.value === "" || input?.value == null) {
+    alert("Please add a task");
+    return;
+  }
+  const newTask: Task = {
+    id: uuidv4(),
+    title: input.value,
+    completed: true, //!checkbox.checked nastimamo na true ali false je obklukan okenček ali pa ne
+    createdAt: Date.now(),
+  };
+  task.push(newTask);
+  // task.push(task);
+  addListItem(newTask);
+  input.value = "";
+});
+//!install npm i uuid če typescript neprepozna types
+
+function addListItem(task: Task) {
+  const item = document.createElement("li");
+  const label = document.createElement("label");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.checked = task.completed;
+  label.append(checkbox, task.title);
+  item.append(label);
+  list?.append(item);
+}
